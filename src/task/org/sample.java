0096786b2261ee12447ce3cde46a96b80c146148
krithickvasantwin11 <krithickvasantwin11@gmail.com>
2022-08-13 23:29:49 +0530

    update code

--- src/task/org/sample.java
+++ src/task/org/sample.java
@@ -23,7 +23,7 @@ public class  sample {
 			}
 			
 		}
-		System.out.println("asc"+"change");
+		System.out.println("asc"+"change new");
 		for (int i = 0; i < a.length; i++) {
 			System.out.println(a[i]+"value");
 		}
